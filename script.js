a700afc0d69d475e473bea09f6ef94321be19c71
Bunjee <abolaji.awoyomi@csedge.org>
2021-08-18 16:16:53 -0400

    portfolio display

---
+++ script.js
@@ -0,0 +1,35 @@
+function dropdown1() {
+    var x = document.getElementById("reveal1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function dropdown2() {
+    var x = document.getElementById("reveal2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function dropdown3() {
+    var x = document.getElementById("reveal3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function dropdown4() {
+    var x = document.getElementById("reveal4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
